d9ba72e7b7805a8446c0fad24c03186b51485dd5
Vaishali1225 <vaishali.yadav@qainfotech.com>
2019-06-14 15:50:22 +0530

    Find Clumps in array

---
+++ Clumps.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Clumps 
+{
+public static void main(String[] args)
+{
+	boolean flag=false;
+	int clumps=0;
+	Scanner sc = new Scanner(System.in);
+	//System.out.println("Enter length of Array:");
+	//int len= sc.nextInt();
+	int a[]=new int[10];
+	System.out.println("Enter elements of Array:");
+	
+	for(int i=0;i<10;i++)
+	{
+		a[i]=sc.nextInt();
+	}
+	for(int i=0;i<10;i++)
+	{
+		int j=i+1;
+		if(a[i]==a[j])
+		{
+			clumps++;
+			while((a[i]==a[j]))
+				{
+				if(j==9)
+					break;
+				j++;
+				
+				}
+		}
+		i=j-1;
+		if(i==8)
+			break;
+		
+		
+	}
+	
+	System.out.println("The number of clumps are "+clumps);
+}
+}
